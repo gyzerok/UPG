c52284a04bfcb4cfe1db6ea30b34c2e784a58924
Fyodor Nezhivoy <gyzerok@gmail.com>
2013-01-07 17:22:42 +0600

    Добавил ; после объявления enum - убрал 100500 ошибок :D

--- UPG/Game.cpp
+++ UPG/Game.cpp
@@ -9,6 +9,7 @@ Game::Game(QString name, QString pass, int maxUsers)
     m_pass = pass;
     m_maxUsers = maxUsers;
     m_gid = generateGid();
+    m_host = NULL;
 }
 
 int Game::getGid()
@@ -16,9 +17,32 @@ int Game::getGid()
     return m_gid;
 }
 
-QString Game::getName()
+Error Game::getState(Game::GameState &state)
 {
-    return m_name;
+    state = m_state;
+
+    return SUCCESS;
+}
+
+Error Game::getName(QString& name)
+{
+    name = m_name;
+
+    return SUCCESS;
+}
+
+Error Game::getHost(User* user)
+{
+    user = m_host;
+
+    return SUCCESS;
+}
+
+Error Game::setWord(QString word)
+{
+    m_word = word;
+
+    return SUCCESS;
 }
 
 bool Game::checkPass(QString pass)
@@ -26,18 +50,37 @@ bool Game::checkPass(QString pass)
     return m_pass == pass;
 }
 
+Error Game::getUsers(QList<User*>& users)
+{
+    QList<User*> result;
+    result.append(m_players);
+    result.append(m_observers);
+
+    users = result;
+
+    return SUCCESS;
+}
+
+Error Game::getPlayers(QList<User*>& players)
+{
+    players = m_players;
+
+    return SUCCESS;
+}
+
 Error Game::addUser(User *user)
 {
-    if (m_users.count() > m_maxUsers) return GAME_IS_FULL;
+    if (m_players.count() + m_observers.count() > m_maxUsers) return GAME_IS_FULL;
 
-    m_users.append(user);
+    m_observers.append(user);
+
     return SUCCESS;
 }
 
 void* Game::chooseHost()
 {
-    int temp = randomInt(0,m_users.count()-1);
-    return m_users.value(temp)->getSocket();
+    int temp = randomInt(0, m_players.count() - 1);
+    return m_players.value(temp)->getSocket();
 }
 
 int Game::randomInt(int low, int high)
@@ -48,8 +91,9 @@ int Game::randomInt(int low, int high)
 int Game::generateGid()
 {
     Registry* registry = Registry::instance();
+
     int temp = randomInt(0,500000);
-    if ( registry->m_games.contains(temp) )
+    if (registry->isGameExist(temp))
         return generateGid();
     else
         return temp;
--- UPG/Game.h
+++ UPG/Game.h
@@ -9,25 +9,32 @@
 class Game
 {
 public:
-
     enum GameState
     {
         PREGAME,
         GAME_STARTED,
         WORD_OFFERED,
         CONTACT
-    }
+    };
 
     Game(QString name, QString pass, int maxUsers = 10);
-    int         getGid();
-    QString     getName();
-    bool        checkPass(QString pass);
-    Error       addUser(User* user);
 
-    void*       chooseHost();
+    int             getGid();
+    Error           getState(GameState& state);
+    Error           getName(QString& name);
+    Error           getHost(User* user);
+    Error           getUsers(QList<User*>& users);
+    Error           getPlayers(QList<User*>& players);
+
+    Error           addUser(User* user);
+    Error           setWord(QString word);
+
+    bool            checkPass(QString pass);
+
+    void*           chooseHost();
     //void setHost(void);
 
-    GameState m_state;
+    GameState       m_state;
 
 
 private:
@@ -38,7 +45,7 @@ private:
     QString         m_name;
     QString         m_pass;
     int             m_maxUsers;
-    QList<User*>    m_users;
+    QList<User*>    m_observers;
 
     QList<User*>    m_players;
     QString         m_word;
