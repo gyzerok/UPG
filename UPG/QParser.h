00b00a8f4e88a59e47df89a3659c0c619f4e05b9
Fyodor Nezhivoy <gyzerok@gmail.com>
2013-01-07 16:39:23 +0600

    parser fixes

--- UPG/QParser.h
+++ UPG/QParser.h
@@ -1,5 +1,5 @@
-#ifndef PARSER_H
-#define PARSER_H
+#ifndef QPARSER_H
+#define QPARSER_H
 #include <QtCore>
 #include <QString>
 #include "Error.h"
@@ -8,7 +8,7 @@ class QParser
 {
 public:
     QParser(QString *request);
-    Error Next (QString *tag, QString *value);
+    Error next(QString *tag, QString *value);
     static QString ParseResponse();
 private:
     QString m_request;
@@ -16,4 +16,4 @@ private:
     bool m_opened;
 };
 
-#endif // PARSER_H
+#endif // QPARSER_H
